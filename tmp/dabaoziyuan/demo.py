a0bf0af3d3fdf98849d93fb48710fc6b818e2381
shangxianw <shangxianw123>
2020-07-18 17:57:33 +0800

    打包

--- tmp/dabaoziyuan/demo.py
+++ tmp/dabaoziyuan/demo.py
@@ -1,7 +1,22 @@
+import sys
 import os
 
-path = "./resource/config.txt"
-with open(path, "r", encoding="utf-8") as f:
+#生成资源文件目录访问路径
+def resource_path(relative_path):
+    if getattr(sys, 'frozen', False): #是否Bundle Resource
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+path1 = resource_path(os.path.join("resource","config.txt"))
+with open(path1, "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
+    input("exit")
+
+path2 = "./config2.txt"
+with open(path2, "r", encoding="utf-8") as f:
     content = f.read()
     print(content)
     input("exit")
--- tmp/dabaoziyuan/test2/demo.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "./resource/config.txt"
-with open(path, "r", encoding="utf-8") as f:
-    content = f.read()
-    print(content)
-    input("exit")
